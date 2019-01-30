70d0d63ba28299962863b1097e3fe2d8f04f09ce
Nicolas Bermudez <bermnicolas@gmail.com>
2019-01-30 16:07:09 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -25,7 +25,7 @@ end
 
 def kesha_maker(array)
   array.collect do |string|
-    string.each {|chars, index| index == 2 ? chars = '$' : chars}
+    string.eeach_with_index {|chars, index| index == 2 ? chars = '$' : chars}
 
   end
 end
